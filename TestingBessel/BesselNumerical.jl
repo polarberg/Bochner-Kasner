b5e6c86654129d07a654e01c12e85cc13c76d988
Austin Nguyen <austinnguyen153@gmail.com>
2020-03-16 16:16:39 -0400

    Bessel Numerical Words

--- TestingBessel/BesselNumerical.jl
+++ TestingBessel/BesselNumerical.jl
@@ -1,5 +1,5 @@
 import Pkg; using Pkg
-Pkg.add("DifferentialEquations"); Pkg.add("Plots"); Pkg.add("Calculus")
+Pkg.add("DifferentialEquations"); Pkg.add("Plots"); Pkg.add("SpecialFunctions") Pkg.add("Calculus")
 using DifferentialEquations, Plots, SpecialFunctions, Calculus
 
 
@@ -8,14 +8,6 @@ function besseleq(dv,v,y,p,t)
     dv[1] = -v[1]/t - ( 1 - a^2/t^2 ) * y[1]  
     #Diffeq *** y'' = -y'/t - ( t^2 -a^2 ) * y
 end
-
-nu=1
-besselj(1,1)
-bessely(1,1)
-
-differentiate()
-
-
 
 a=1.0
 tspan=(1.0,10.0)
@@ -30,5 +22,13 @@ function solArray(somesol)
     return z 
 end 
 
-sol=solve(SecondOrderODEProblem(besseleq,[du0],[u0],tspan,(a)),DP)
+beq=SecondOrderODEProblem(besseleq,[du0], [u0], tspan, a)
+sol=solve(beq,DPRKN12(),dense=true,abstol=1e-20,reltol=1e-20, dt=1.0, maxiters=1e50)
 plot(sol.t,solArray(sol))
+
+
+#nu=1
+#besselj(1,1)
+#bessely(1,1)
+#
+#differentiate()
